d59a8ea2f3401d653119ce0e4a55ac03c0fe4ca0
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-11-06 18:06:23 -0800

    Fix aur and arch package url installer scripts.

--- setup-arch.sh
+++ setup-arch.sh
@@ -8,13 +8,13 @@ if hash paru; then
 else
   pacman -S --needed --noconfirm $arch_packages
   for package in $aur_packages; do
-    "$(dirname -- "$0")/install-aur-makepkg.sh" $package
+    "$HOME/dotfiles/main/.local/bin/install-aur-makepkg.sh" $package
   done
 fi
 
 # Install packages from URL
 for url in $arch_package_urls; do
-  "$(dirname -- "$0")/install-arch-pkg-from-url.sh" $url
+  "$HOME/dotfiles/main/.local/bin/install-arch-pkg-from-url.sh" $url
 done
 
 # Install pipx packages
